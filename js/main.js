06767209cc1bd9137c7cc6eca103bc89a801b4c7
Davis <zhongdavis2001@gmail.com>
2025-06-02 00:14:21 -0700

    test

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// js/main.js
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  });
+  
+  document.querySelectorAll('.fade-in').forEach((el) => observer.observe(el));
+  
